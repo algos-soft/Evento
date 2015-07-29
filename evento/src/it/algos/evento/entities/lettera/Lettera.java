81b0d764374acf090bfdf74c33773b844528b170
GuidoCeresa <gac@algos.it>
2015-07-29 10:07:21 +0200

    .

--- evento/src/it/algos/evento/entities/lettera/Lettera.java
+++ evento/src/it/algos/evento/entities/lettera/Lettera.java
@@ -1,223 +1,209 @@
 package it.algos.evento.entities.lettera;
 
+import it.algos.evento.entities.spedizione.Spedizione;
 import it.algos.evento.multiazienda.EQuery;
 import it.algos.evento.multiazienda.EventoEntity;
 import it.algos.evento.multiazienda.EventoEntityQuery;
 import it.algos.web.entity.BaseEntity;
-import it.algos.evento.entities.spedizione.Spedizione;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import it.algos.web.query.AQuery;
+import org.eclipse.persistence.annotations.CascadeOnDelete;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
-
-import it.algos.web.query.AQuery;
-import org.eclipse.persistence.annotations.CascadeOnDelete;
-import org.hibernate.validator.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 @Entity
 public class Lettera extends EventoEntity {
 
-	private static final long serialVersionUID = 5232130836430670798L;
+    private static final long serialVersionUID = 5232130836430670798L;
+    public static EventoEntityQuery<Lettera> query = new EventoEntityQuery(Lettera.class);
 
-	@NotEmpty
-	@Size(min = 2, max = 30)
-	private String sigla = "";
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    private String sigla = "";
 
-	@Size(min = 2, max = 80)
-	private String oggetto = "";
+    @Size(min = 2, max = 80)
+    private String oggetto = "";
 
-	@Column(length = 16384)
-	private String testo = "";
+    @Column(length = 16384)
+    private String testo = "";
 
-	private String allegati = "";
-	
-    @OneToMany(mappedBy="lettera")
+    private String allegati = "";
+
+    @OneToMany(mappedBy = "lettera")
     @CascadeOnDelete
-	private Set<Spedizione> spedizioni;
+    private Set<Spedizione> spedizioni;
 
-	private boolean html = false;
-
-	
-	public static EventoEntityQuery<Lettera> query = new EventoEntityQuery(Lettera.class);
-
-	
-	
-	public Lettera() {
-		this("", "", "");
-	}// end of constructor
-
-	public Lettera(String sigla, String oggetto, String testo) {
-		this(sigla, oggetto, testo, "");
-	}// end of constructor
-
-	public Lettera(String sigla, String oggetto, String testo, String allegati) {
-		super();
-		this.setSigla(sigla);
-		this.setOggetto(oggetto);
-		this.setTesto(testo);
-		this.setAllegati(allegati);
-	}// end of constructor
-
-	@Override
-	public String toString() {
-		return getSigla();
-	}// end of method
-
-	/**
-	 * @return the sigla
-	 */
-	public String getSigla() {
-		return sigla;
-	}
-
-	/**
-	 * @param sigla
-	 *            the sigla to set
-	 */
-	public void setSigla(String sigla) {
-		this.sigla = sigla;
-	}
-
-	/**
-	 * @return the oggetto
-	 */
-	public String getOggetto() {
-		return oggetto;
-	}
-
-	/**
-	 * @param oggetto
-	 *            the oggetto to set
-	 */
-	public void setOggetto(String oggetto) {
-		this.oggetto = oggetto;
-	}
-
-	/**
-	 * @return the testo
-	 */
-	public String getTesto() {
-		return testo;
-	}
-
-	/**
-	 * @param testo
-	 *            the testo to set
-	 */
-	public void setTesto(String testo) {
-		this.testo = testo;
-	}
-
-	/**
-	 * @return the allegati
-	 */
-	public String getAllegati() {
-		return allegati;
-	}
-
-	/**
-	 * @param allegati
-	 *            the allegati to set
-	 */
-	public void setAllegati(String allegati) {
-		this.allegati = allegati;
-	}
-	
-	/**
-	 * @return the spedizioni
-	 */
-	public Set<Spedizione> getSpedizioni() {
-		return spedizioni;
-	}
-
-	/**
-	 * @param spedizioni the spedizioni to set
-	 */
-	public void setSpedizioni(Set<Spedizione> spedizioni) {
-		this.spedizioni = spedizioni;
-	}
+    private boolean html = false;
 
 
-	public boolean isHtml() {
-		return html;
-	}
+    public Lettera() {
+        this("", "", "");
+    }// end of constructor
 
+    public Lettera(String sigla, String oggetto, String testo) {
+        this(sigla, oggetto, testo, "");
+    }// end of constructor
 
-	public void setHtml(boolean html) {
-		this.html = html;
-	}
+    public Lettera(String sigla, String oggetto, String testo, String allegati) {
+        super();
+        this.setSigla(sigla);
+        this.setOggetto(oggetto);
+        this.setTesto(testo);
+        this.setAllegati(allegati);
+    }// end of constructor
 
+    public static void sostituisce(String sigla, String oggetto, String testo) {
+        Lettera lettera = readBySigla(sigla);
 
-	public static void sostituisce(String sigla, String oggetto, String testo) {
-		Lettera lettera = readBySigla(sigla);
+        if (lettera != null) {
+            lettera.delete();
+        }// end of if cycle
+        new Lettera(sigla, oggetto, testo).save();
 
-		if (lettera != null) {
-			lettera.delete();
-		}// end of if cycle
-		new Lettera(sigla, oggetto, testo).save();
+    }// end of method
 
-	}// end of method
+    public static Lettera readBySigla(String sigla) {
+        Lettera lettera = null;
+        BaseEntity bean = null;
 
+        bean = EQuery.queryFirst(Lettera.class, Lettera_.sigla, sigla);
 
-	public static Lettera readBySigla(String sigla) {
-		Lettera lettera = null;
-		BaseEntity bean = null;
+        if (bean != null) {
+            if (bean instanceof Lettera) {
+                lettera = (Lettera) bean;
+            }// end of if cycle
+        }// end of if cycle
 
-		bean = EQuery.queryFirst(Lettera.class, Lettera_.sigla, sigla);
+        return lettera;
+    }// end of method
 
-		if (bean != null) {
-			if (bean instanceof Lettera) {
-				lettera = (Lettera) bean;
-			}// end of if cycle
-		}// end of if cycle
+    /**
+     * Recupera una istanza di Lettera usando la query specifica
+     *
+     * @return istanza di Lettera, null se non trovata
+     */
+    public static Lettera find(long id) {
+        Lettera instance = null;
+        BaseEntity entity = AQuery.queryById(Lettera.class, id);
 
-		return lettera;
-	}// end of method
+        if (entity != null) {
+            if (entity instanceof Lettera) {
+                instance = (Lettera) entity;
+            }// end of if cycle
+        }// end of if cycle
 
-	/**
-	 * Recupera una istanza di Lettera usando la query specifica
-	 *
-	 * @return istanza di Lettera, null se non trovata
-	 */
-	public static Lettera find(long id) {
-		Lettera instance = null;
-		BaseEntity entity = AQuery.queryById(Lettera.class, id);
+        return instance;
+    }// end of method
 
-		if (entity != null) {
-			if (entity instanceof Lettera) {
-				instance = (Lettera) entity;
-			}// end of if cycle
-		}// end of if cycle
+    public static ArrayList<Lettera> readAll() {
+        ArrayList<Lettera> lista = null;
 
-		return instance;
-	}// end of method
+        try { // prova ad eseguire il codice
+            lista = (ArrayList<Lettera>) EQuery.getList(Lettera.class);
+        } catch (Exception unErrore) { // intercetta l'errore
+        }// fine del blocco try-catch
 
-	public static ArrayList< Lettera> readAll() {
-		ArrayList<Lettera> lista = null;
+        return lista;
+    }// end of method
 
-		try { // prova ad eseguire il codice
-			lista = (ArrayList<Lettera>) EQuery.getList(Lettera.class);
-		} catch (Exception unErrore) { // intercetta l'errore
-		}// fine del blocco try-catch
+    @Override
+    public String toString() {
+        return getSigla();
+    }// end of method
 
-		return lista;
-	}// end of method
+    /**
+     * @return the sigla
+     */
+    public String getSigla() {
+        return sigla;
+    }
 
+    /**
+     * @param sigla the sigla to set
+     */
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
 
-	public String getTestOut(HashMap<String, String> mappaEscape) {
-		String testoOut = "";
-		String testoIn = "";
+    /**
+     * @return the oggetto
+     */
+    public String getOggetto() {
+        return oggetto;
+    }
 
-		testoIn = this.testo;
-		testoOut = LetteraService.getTesto(testoIn, mappaEscape);
+    /**
+     * @param oggetto the oggetto to set
+     */
+    public void setOggetto(String oggetto) {
+        this.oggetto = oggetto;
+    }
 
-		return testoOut;
-	}// end of method
-	
+    /**
+     * @return the testo
+     */
+    public String getTesto() {
+        return testo;
+    }
+
+    /**
+     * @param testo the testo to set
+     */
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+    /**
+     * @return the allegati
+     */
+    public String getAllegati() {
+        return allegati;
+    }
+
+    /**
+     * @param allegati the allegati to set
+     */
+    public void setAllegati(String allegati) {
+        this.allegati = allegati;
+    }
+
+    /**
+     * @return the spedizioni
+     */
+    public Set<Spedizione> getSpedizioni() {
+        return spedizioni;
+    }
+
+    /**
+     * @param spedizioni the spedizioni to set
+     */
+    public void setSpedizioni(Set<Spedizione> spedizioni) {
+        this.spedizioni = spedizioni;
+    }
+
+    public boolean isHtml() {
+        return html;
+    }
+
+    public void setHtml(boolean html) {
+        this.html = html;
+    }
+
+    public String getTestOut(HashMap<String, String> mappaEscape) {
+        String testoOut = "";
+        String testoIn = "";
+
+        testoIn = this.testo;
+        testoOut = LetteraService.getTesto(testoIn, mappaEscape);
+
+        return testoOut;
+    }// end of method
+
 
 }// end of entity class
